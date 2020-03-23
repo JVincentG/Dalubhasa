449d9fe7530800f53ee8de057d6745ea09a83b3d
Ragnov <59752567+Ragnov@users.noreply.github.com>
2020-03-23 19:01:54 +0800

    3-23-2020 updated codes

---
+++ codes/p1win.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dalubhasa03
+{
+    public partial class p1win : Form
+    {
+        public p1win()
+        {
+            InitializeComponent();
+            axWindowsMediaPlayer1.URL = "p1win sfx.wav";
+        }
+
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Form1 myform = new Form1();
+            myform.Show();
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MainMenucs myforms = new MainMenucs();
+            myforms.Show();
+            this.Hide();
+        }
+    }
+}
